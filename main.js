2ee6c11ad89c565e15c4e86ba65ff6d3fbcc3d12
L.M. Wambugu <lewiswambugu01@gmail.com>
2022-09-22 23:03:09 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+const tabItems = document.querySelectorAll('.tab-item');
+const tabContentItems = document.querySelectorAll('.tab-content-item');
+
+// Select tab content item
+
+function selectItem(e) {
+    // Remove border
+    removeBorder()
+    removeShow()
+        // Add border to currect tab
+    this.classList.add('tab-border');
+
+    // Grab content item from DOM
+    const tabContentItems = document.querySelector(`#${this.id}-content`)
+        // Add show class
+    tabContentItems.classList.add('show')
+}
+
+function removeBorder() {
+    tabItems.forEach(item => item.classList.remove('tab-border'))
+}
+
+function removeShow() {
+    tabContentItems.forEach(item => item.classList.remove('show'))
+}
+// Listen to tab click
+tabItems.forEach(item => item.addEventListener('click', selectItem));
